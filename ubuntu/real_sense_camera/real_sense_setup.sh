4d3171ff1d988cf4422cecc3b2f552478afa1492
Keya87 <pinocchio.madscience@gmail.com>
2019-03-04 20:09:47 +0200

    Workaround for realsense to install kmodules

--- ubuntu/real_sense_camera/real_sense_setup.sh
+++ ubuntu/real_sense_camera/real_sense_setup.sh
@@ -4,6 +4,11 @@
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
 
+# Create symbolic link for DKMS to install the modules
+# Workaround - Hardcode kernel version 
+# Applied since my linux headers does not match with the modules version  
+sudo ln -s /usr/src/linux-headers-4.15.0-45-generic  /lib/modules/4.15.0-37-generic/build 
+
 sudo apt-get install -y librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg
 
 # Install the ROS Melodic distribution
