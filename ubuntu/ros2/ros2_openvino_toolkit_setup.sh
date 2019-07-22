6e8aa270e88e5e9f3ff7665ce2512f30150c6ac5
keya87 <pinocchio.madscience@gmail.com>
2019-07-22 13:14:14 +0300

    Update scripts for Openvino 2019_R1.1

--- ubuntu/ros2/ros2_openvino_toolkit_setup.sh
+++ ubuntu/ros2/ros2_openvino_toolkit_setup.sh
@@ -103,34 +103,34 @@ sudo mkdir /opt/openvino_toolkit/
 mkdir ~/code && cd ~/code
 git clone https://github.com/opencv/dldt.git
 cd dldt/inference-engine/
-git checkout 2018_R5
+git checkout 2019_R1.1
 git submodule init
 git submodule update --recursive
-cp -r $HOME/upboard-linux-ia/ubuntu/ros2/scripts/dldt/install_dependencies.sh ~/code/dldt/inference-engine/
 ./install_dependencies.sh
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_GNA=OFF ..
-make -j8
+cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_GNA=OFF -DENABLE_MKL_DNN=OFF ..
+sudo make -j8
 sudo mkdir -p /opt/openvino_toolkit
 sudo ln -sf ~/code/dldt /opt/openvino_toolkit/dldt
+ln -sf /home/keya87/code/dldt/inference-engine/bin/intel64/Release/lib/ /opt/openvino_toolkit/deployment_tools/inference_engine/samples/build/intel64/Release
 
 # Install Open Model Zoo
 cd ~/code
 git clone https://github.com/opencv/open_model_zoo.git
 cd open_model_zoo/demos/
-git checkout 2018_R5
+git checkout 2019
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release /opt/openvino_toolkit/dldt/inference-engine -DENABLE_GNA=OFF
-make -j8
+cmake -DCMAKE_BUILD_TYPE=Release /opt/openvino_toolkit/dldt/inference-engine -DENABLE_GNA=OFF -DENABLE_MKL_DNN=OFF
+sudo make -j8
 sudo mkdir -p /opt/openvino_toolkit
 sudo ln -sf ~/code/open_model_zoo /opt/openvino_toolkit/open_model_zoo
-
-export CPU_EXTENSION_LIB=/opt/openvino_toolkit/deployment_tools/inference_engine/samples/build/intel64/Release/lib/libcpu_extension.so
-export GFLAGS_LIB=/opt/openvino_toolkit/deployment_tools/inference_engine/samples/build/intel64/Release/lib/libgflags_nothreads.a
 
 # set ENV CPU_EXTENSION_LIB and GFLAGS_LIB
 echo "export CPU_EXTENSION_LIB=$HOME/code/dldt/inference-engine/bin/intel64/Release/lib/libcpu_extension.so" >> ~/.bashrc
 echo "export GFLAGS_LIB=$HOME/code/dldt/inference-engine/bin/intel64/Release/lib/libgflags_nothreads.a" >> ~/.bashrc
+echo "export InferenceEngine_DIR=/opt/openvino_toolkit/dldt/inference-engine/build/" >> ~/.bashrc
+echo "export OpenCV_DIR=$HOME/code/opencv/build" >> ~/.bashrc
+echo "source ~/ros2_ws/install/local_setup.bash" >> ~/.bashrc
 
 
 # Install ROS2_OpenVINO packages
@@ -142,30 +142,42 @@ git clone https://github.com/ros-perception/vision_opencv -b ros2
 git clone https://github.com/ros2/message_filters.git
 git clone https://github.com/ros-perception/image_common.git -b ros2
 git clone https://github.com/intel/ros2_intel_realsense.git
+# Building and Installation the samples
+echo "ROOT is required instead of sudo"
+echo "Type sudo su and enter next commands"
+
+echo "mkdir -p /root/code/dldt/inference-engine/bin/intel64/Release/"
+echo "ln -sf /home/keya87/code/dldt/inference-engine/bin/intel64/Release/lib/ /root/code/dldt/inference-engine/bin/intel64/Release/"
+echo "export InferenceEngine_DIR=/opt/openvino_toolkit/dldt/inference-engine/build/"
+echo "export CPU_EXTENSION_LIB=$HOME/code/dldt/inference-engine/bin/intel64/Release/lib/libcpu_extension.so"
+echo "export GFLAGS_LIB=$HOME/code/dldt/inference-engine/bin/intel64/Release/lib/libgflags_nothreads.a"
+echo "export OpenCV_DIR=$HOME/code/opencv/build"
 
 # Build package
-echo "source ~/ros2_ws/install/local_setup.bash" >> ~/.bashrc
-source ~/ros2_ws/install/local_setup.bash
-source /opt/intel/openvino_*/bin/setupvars.sh
-export OpenCV_DIR=$HOME/code/opencv/build
-echo "export OpenCV_DIR=$HOME/code/opencv/build" >> ~/.bashrc
+echo "source ~/ros2_ws/install/local_setup.bash"
+echo "source /opt/intel/openvino_*/bin/setupvars.sh"
+
 
 # Workaround some headers from next package are needed for compile
-sudo apt-get install ros-crystal-librealsense2
+# sudo apt-get install ros-crystal-librealsense2 ??
 
-cd ~/ros2_overlay_ws
-sudo colcon build --symlink-install
+echo "cd ~/ros2_overlay_ws"
+echo "colcon build --symlink-install"
 echo "source ~/ros2_overlay_ws/install/local_setup.bash" >> ~/.bashrc
+
+
+#PART 2
+cd ~/ros2_overlay_ws
 source ./install/local_setup.bash
 sudo mkdir -p /opt/openvino_toolkit
 sudo ln -sf ~/ros2_overlay_ws/src/ros2_openvino_toolkit /opt/openvino_toolkit/ros2_openvino_toolkit
 
 sudo apt-get remove -y ros-crystal-librealsense2
 # Workaround for creating missing test files
-sudo mkdir -p /home/keya87/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/ogre_media_resources/scripts
-sudo mkdir -p /home/keya87/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/ogre_media_resources/meshes
-cp ~/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/scripts/test /home/keya87/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/ogre_media_resources/scripts
-cp ~/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/meshes/test /home/keya87/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/ogre_media_resources/meshes
+#sudo mkdir -p /home/keya87/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/#ogre_media_resources/scripts
+#sudo mkdir -p /home/keya87/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/ogre_media_resources/meshes
+#cp ~/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/scripts/test /home/keya87/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/ogre_media_resources/scripts
+#cp ~/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/meshes/test /home/keya87/ros2_ws/install/rviz_rendering_tests/share/rviz_rendering_tests/ogre_media_resources/meshes
 
 # Running the Demo
 
@@ -176,17 +188,18 @@ mkdir -p ~/Downloads/models
 cd ~/Downloads/models
 wget http://download.tensorflow.org/models/object_detection/mask_rcnn_inception_v2_coco_2018_01_28.tar.gz
 tar -zxvf mask_rcnn_inception_v2_coco_2018_01_28.tar.gz
+sudo chmod -R mask_rcnn_inception_v2_coco_2018_01_28
 cd mask_rcnn_inception_v2_coco_2018_01_28
 python3 /opt/intel/openvino_*/deployment_tools/model_optimizer/mo_tf.py --input_model frozen_inference_graph.pb --tensorflow_use_custom_operations_config /opt/intel/openvino_*/deployment_tools/model_optimizer/extensions/front/tf/mask_rcnn_support.json --tensorflow_object_detection_api_pipeline_config pipeline.config --reverse_input_channels --output_dir ./output/
 sudo mkdir -p /opt/models
 sudo ln -sf ~/Downloads/models/mask_rcnn_inception_v2_coco_2018_01_28 /opt/models/
 #object detection model
-cd /opt/intel/openvino_*/deployment_tools/model_downloader
+cd /opt/intel/openvino_*/deployment_tools/tools/model_downloader
 sudo python3 ./downloader.py --name mobilenet-ssd
 #FP32 precision model
-sudo python3 /opt/intel/openvino_*/deployment_tools/model_optimizer/mo.py --input_model /opt/intel/openvino_*/deployment_tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/mobilenet-ssd.caffemodel --output_dir /opt/intel/openvino_*/deployment_tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP32 --mean_values [127.5,127.5,127.5] --scale_values [127.5]
+sudo python3 /opt/intel/openvino_*/deployment_tools/model_optimizer/mo.py --input_model /opt/intel/openvino_*/deployment_tools/tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/mobilenet-ssd.caffemodel --output_dir /opt/intel/openvino_*/deployment_tools/toolsmodel_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP32 --mean_values [127.5,127.5,127.5] --scale_values [127.5]
 #FP16 precision model
-sudo python3 /opt/intel/openvino_*/deployment_tools/model_optimizer/mo.py --input_model /opt/intel/openvino_*/deployment_tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/mobilenet-ssd.caffemodel --output_dir /opt/intel/openvino_*/deployment_tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP16 --data_type=FP16 --mean_values [127.5,127.5,127.5] --scale_values [127.5]
+sudo python3 /opt/intel/openvino_*/deployment_tools/model_optimizer/mo.py --input_model /opt/intel/openvino_*/deployment_tools/tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/mobilenet-ssd.caffemodel --output_dir /opt/intel/openvino_*/deployment_tools/tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP16 --data_type=FP16 --mean_values [127.5,127.5,127.5] --scale_values [127.5]
 
 # copy label files (excute once)
 sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/emotions-recognition/FP32/emotions-recognition-retail-0003.labels /opt/intel/openvino_*/deployment_tools/intel_models/emotions-recognition-retail-0003/FP32
